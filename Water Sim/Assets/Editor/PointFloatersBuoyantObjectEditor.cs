124b8efeac30ca96f0389fa81a4e68605c0e9ba7
Jeremy Fischer <63757124+Jflyer45@users.noreply.github.com>
2025-04-24 19:02:18 -0700

    Editor script for floating points

---
+++ Water Sim/Assets/Editor/PointFloatersBuoyantObjectEditor.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(PointFloatersBuoyantObject))]
+public class PointFloatersBuoyantObjectEditor : Editor
+{
+    void OnSceneGUI()
+    {
+        var comp = (PointFloatersBuoyantObject)target;
+        var tf = comp.transform;
+        var points = comp.localFloatPoints;
+
+        EditorGUI.BeginChangeCheck();
+        for (int i = 0; i < points.Length; i++)
+        {
+            // calculate world position
+            var worldPos = tf.TransformPoint(points[i]);
+            // draw a position handle
+            var newWorldPos = Handles.PositionHandle(worldPos, Quaternion.identity);
+            if (EditorGUI.EndChangeCheck())
+            {
+                Undo.RecordObject(comp, "Move Float Point");
+                // convert back to local space
+                points[i] = tf.InverseTransformPoint(newWorldPos);
+                comp.localFloatPoints = points;
+                EditorUtility.SetDirty(comp);
+            }
+        }
+    }
+}
